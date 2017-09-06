15101d5be67ba7c55a23f1e52c122df9f02f42d1
Dave Brothers <Dave.Brothers@Milliken.com>
2017-09-05 21:05:48 -0400

    initial commit.

---
+++ Plating/CassetteConfiguration.cs
@@ -0,0 +1,32 @@
+using Cassette;
+using Cassette.Scripts;
+using Cassette.Stylesheets;
+
+namespace Plating
+{
+    /// <summary>
+    /// Configures the Cassette asset bundles for the web application.
+    /// </summary>
+    public class CassetteBundleConfiguration : IConfiguration<BundleCollection>
+    {
+        public void Configure(BundleCollection bundles)
+        {
+            var scriptsFiles = new[]
+            {
+                "Content/Scripts/jquery-1.9.1.min.js",
+                "Content/Scripts/bootstrap.min.js"
+            };
+
+            var stylesFileSearch = new FileSearch
+            {
+                Pattern = "*.min.css"
+            };
+
+            //bundles.AddPerSubDirectory<ScriptBundle>(applicationRelativePath: "Content", fileSearch: scriptsFileSearch);
+            bundles.Add<ScriptBundle>("Scripts", scriptsFiles);
+            bundles.AddPerSubDirectory<StylesheetBundle>(applicationRelativePath: "Content", fileSearch: stylesFileSearch);
+            //bundles.AddPerIndividualFile<ScriptBundle>(applic: "Scripts", fileSearch: scriptsFileSearch);
+            //bundles.AddPerIndividualFile<StylesheetBundle>(directoryPath: "Content", fileSearch: stylesFileSearch);
+        }
+    }
+}
